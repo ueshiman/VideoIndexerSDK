1c512f0541bb437979125c986875e4eb4467a09d
tateno <masa@tateno.net>
2025-06-10 05:52:30 +0900

    ロゴグループ作成機能を追加
    `CreateLogoRepository` にロゴグループ作成メソッドを追加し、関連するマッピングインターフェースとモデルを新規作成しました。エラーログメッセージの修正も行いました。

---
+++ VideoIndexerAccess/Repositories/DataModelMapper/LogoGroupResponseMapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoIndexerAccessCore.VideoIndexerClient.ApiModel;
+
+namespace VideoIndexerAccess.Repositories.DataModelMapper
+{
+    public class LogoGroupResponseMapper : ILogoGroupResponseMapper
+    {
+        public LogoGroupResponseModel MapFrom(ApiLogoGroupResponseModel model)
+        {
+            return new LogoGroupResponseModel
+            {
+                Id = model.id,
+                Name = model.name,
+                Description = model.description,
+                CreatedBy = model.createdBy
+            };
+        }
+
+        public ApiLogoGroupResponseModel MapToApiLogoGroupResponseModel(LogoGroupResponseModel model)
+        {
+            return new ApiLogoGroupResponseModel
+            {
+                id = model.Id,
+                name = model.Name,
+                description = model.Description,
+                createdBy = model.CreatedBy
+            };
+        }
+
+    }
+}
--- VideoIndexerAccess/Repositories/VideoItemRepository/CreateLogoRepository.cs
+++ VideoIndexerAccess/Repositories/VideoItemRepository/CreateLogoRepository.cs
@@ -33,10 +33,11 @@ namespace VideoIndexerAccess.Repositories.VideoItemRepository
 
         private readonly ILogoResponseMapper _logoResponseMapper;
         private readonly ILogoRequestMapper _logoRequestMapper;
+        private readonly ILogoGroupResponseMapper _logoGroupResponseMapper;
         // 例外スロー時のパラメータ名
         private const string ParamName = "createLogo";
 
-        public CreateLogoRepository(ILogger<CreateLogoRepository> logger, IAuthenticationTokenizer authenticationTokenizer, IAccounApitAccess accountAccess, IAccountRepository accountRepository, ICustomLogosApiAccess customLogosApiAccess, IApiResourceConfigurations apiResourceConfigurations, ILogoResponseMapper logoResponseMapper, ILogoRequestMapper logoRequestMapper)
+        public CreateLogoRepository(ILogger<CreateLogoRepository> logger, IAuthenticationTokenizer authenticationTokenizer, IAccounApitAccess accountAccess, IAccountRepository accountRepository, ICustomLogosApiAccess customLogosApiAccess, IApiResourceConfigurations apiResourceConfigurations, ILogoResponseMapper logoResponseMapper, ILogoRequestMapper logoRequestMapper, ILogoGroupResponseMapper logoGroupResponseMapper)
         {
             _logger = logger;
             _authenticationTokenizer = authenticationTokenizer;
@@ -46,6 +47,7 @@ namespace VideoIndexerAccess.Repositories.VideoItemRepository
             _apiResourceConfigurations = apiResourceConfigurations;
             _logoResponseMapper = logoResponseMapper;
             _logoRequestMapper = logoRequestMapper;
+            _logoGroupResponseMapper = logoGroupResponseMapper;
         }
 
         /// <summary>
@@ -110,8 +112,55 @@ namespace VideoIndexerAccess.Repositories.VideoItemRepository
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Unexpected error occurred while creating custom logol: location={Location}, accountId={AccountId}, model={Name}", location, accountId, request.Name);
+                _logger.LogError(ex, "Unexpected error occurred while creating custom logo: location={Location}, accountId={AccountId}, model={Name}", location, accountId, request.Name);
                 throw; // 元の例外をそのまま再スロー
+            }
+        }
+
+        public async Task<LogoGroupResponseModel> CreateLogoGroupAsync(ApiLogoGroupRequestModel request)
+        {
+            // アカウント情報を取得し、存在しない場合は例外をスロー
+            var account = await _accountAccess.GetAccountAsync(_apiResourceConfigurations.ViAccountName) ?? throw new ArgumentNullException(paramName: ParamName);
+
+            // アカウント情報のチェック
+            _accountRepository.CheckAccount(account);
+
+            // アカウントのロケーションとIDを取得
+            string? location = account.location;
+            string? accountId = account.properties?.id;
+
+            // アクセストークンを取得
+            string accessToken = await _authenticationTokenizer.GetAccessToken();
+
+            return await CreateLogoGroupAsync(location!, accountId!, request, accessToken);
+        }
+
+        public async Task<LogoGroupResponseModel> CreateLogoGroupAsync(string location, string accountId, ApiLogoGroupRequestModel request, string? accessToken = null)
+        {
+            try
+            {
+                // API へロゴグループ作成リクエスト送信
+                var apiResponse = await _customLogosApiAccess.CreateLogoGroupAsync(location, accountId, request, accessToken);
+
+                // レスポンスをドメインモデルにマッピング
+                var result = _logoGroupResponseMapper.MapFrom(apiResponse);
+
+                return result;
+            }
+            catch (ArgumentException ex)
+            {
+                _logger.LogError(ex, "Argument error: location={Location}, accountId={AccountId}, groupName={Name}", location, accountId, request.name);
+                throw;
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, "API request failed: location={Location}, accountId={AccountId}, groupName={Name}", location, accountId, request.name);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error occurred while creating logo group: location={Location}, accountId={AccountId}, groupName={Name}", location, accountId, request.name);
+                throw;
             }
         }
     }
