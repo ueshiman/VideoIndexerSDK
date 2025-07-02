b038a6f5fbf08ca0d533d3717a89f109af8b21b0
tateno <masa@tateno.net>
2025-07-03 07:40:39 +0900

    VideosRepositoryクラスを追加
    新しいクラス `VideosRepository` を追加し、ロガーや複数のインターフェースを定義しました。このクラスには、ビデオを削除するための非同期メソッド `DeleteVideoAsync` が含まれていますが、実装は未完了です。APIリソース設定やアカウント情報取得のためのインターフェースを使用する準備が整っています。

---
+++ VideoIndexerAccess/Repositories/VideoItemRepository/VideosRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoIndexerAccess.Repositories.AuthorizAccess;
+using VideoIndexerAccessCore.VideoIndexerClient.ApiAccess;
+using VideoIndexerAccessCore.VideoIndexerClient.ApiModel;
+using VideoIndexerAccessCore.VideoIndexerClient.Configuration;
+
+namespace VideoIndexerAccess.Repositories.VideoItemRepository
+{
+    public class VideosRepository
+    {
+        // ロガーインスタンス
+        private readonly ILogger<VideosRepository> _logger;
+
+        // アクセストークン取得用インターフェース
+        private readonly IAuthenticationTokenizer _authenticationTokenizer;
+
+        // アカウント情報取得用インターフェース
+        private readonly IAccounApitAccess _accountAccess;
+
+        // アカウント検証用リポジトリ
+        private readonly IAccountRepository _accountRepository;
+
+        // APIリソース設定
+        private readonly IApiResourceConfigurations _apiResourceConfigurations;
+
+        // API設定
+        private readonly IVideosApiAccess _videosApiAccess;
+        private readonly IVideoDownloadApiAccess _videoDownloadApiAccess;
+
+        private const string ParamName = "videos";
+
+        // マッパーインターフェース
+
+
+        public async Task<ApiDeleteVideoResultModel?> DeleteVideoAsync(string location, string accountId, string videoId, string? accessToken = null)
+        {
+            // tobo
+            throw new NotImplementedException("DeleteVideoAsync method is not implemented yet.");
+        }
+
+    }
+}
