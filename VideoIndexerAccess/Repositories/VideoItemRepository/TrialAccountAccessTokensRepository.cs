2a80aed67a9da45745a1ba897b1906ac2f864d36
tateno <masa@tateno.net>
2025-07-02 16:47:26 +0900

    アクセストークン取得メソッドを追加
    `TrialAccountAccessTokensRepository.cs` に Microsoft.Identity.Client の名前空間を追加し、ユーザーおよびビデオに対するアクセストークンを取得する新しい非同期メソッド `GetUserAccessTokenAsync` と `GetVideoAccessTokenAsync` を実装しました。これにより、アカウント情報を取得し、必要なアクセストークンを取得する処理が強化されました。各メソッドには、引数や戻り値に関する詳細な説明を含む XML コメントも追加されています。

--- VideoIndexerAccess/Repositories/VideoItemRepository/TrialAccountAccessTokensRepository.cs
+++ VideoIndexerAccess/Repositories/VideoItemRepository/TrialAccountAccessTokensRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -187,5 +188,72 @@ namespace VideoIndexerAccess.Repositories.VideoItemRepository
             // アカウントアクセストークンを取得
             return await _accountAccessTokensApiAccess.GetProjectAccessTokenAsync(location, accountId, projectId, allowEdit, clientRequestId);
         }
+
+        /// <summary>
+        /// ユーザーに対するアクセストークンを取得する非同期メソッド。
+        /// </summary>
+        /// <returns>アクセストークンの文字列。エラー時は null を返す。</returns>
+        public async Task<string?> GetUserAccessTokenAsync()
+        {
+            // アカウント情報を取得し、存在しない場合は例外をスロー
+            var account = await _accountAccess.GetAccountAsync(_apiResourceConfigurations.ViAccountName) ?? throw new ArgumentNullException(paramName: ParamName);
+
+            // アカウント情報のチェック
+            _accountRepository.CheckAccount(account);
+
+            // アカウントのロケーションとIDを取得
+            string? location = account.location;
+
+            // ユーザーアクセストークンを取得
+            return await GetUserAccessTokenAsync(location!, allowEdit: null, clientRequestId: null);
+        }
+
+        /// <summary>
+        /// ユーザーに対するアクセストークンを取得する非同期メソッド。
+        /// </summary>
+        /// <param name="location">Azure リージョン。</param>
+        /// <param name="allowEdit">編集を許可するかどうか（true または false）。省略可。</param>
+        /// <param name="clientRequestId">リクエスト識別用の GUID（省略可）。</param>
+        /// <returns>アクセストークンの文字列。エラー時は null を返す。</returns>
+        public async Task<string?> GetUserAccessTokenAsync(string location, bool? allowEdit = null, string? clientRequestId = null)
+        {
+            return await _accountAccessTokensApiAccess.GetUserAccessTokenAsync(location, allowEdit, clientRequestId);
+        }
+        
+        /// <summary>
+        /// ビデオに対するアクセストークンを取得する非同期メソッド。
+        /// </summary>
+        /// <param name="videoId">ビデオ ID。</param>
+        /// <returns>アクセストークンの文字列。失敗時は null。</returns>
+        public async Task<string?> GetVideoAccessTokenAsync(string videoId)
+        {
+            // アカウント情報を取得し、存在しない場合は例外をスロー
+            var account = await _accountAccess.GetAccountAsync(_apiResourceConfigurations.ViAccountName) ?? throw new ArgumentNullException(paramName: ParamName);
+
+            // アカウント情報のチェック
+            _accountRepository.CheckAccount(account);
+
+            // アカウントのロケーションとIDを取得
+            string? location = account.location;
+            string? accountId = account.properties?.id;
+
+            // ビデオアクセストークンを取得
+            return await GetVideoAccessTokenAsync(location!, accountId!, videoId, allowEdit: null, clientRequestId: null);
+        }
+
+        /// <summary>
+        /// ビデオに対するアクセストークンを取得する非同期メソッド。
+        /// </summary>
+        /// <param name="location">Azure リージョン。</param>
+        /// <param name="accountId">アカウント ID。</param>
+        /// <param name="videoId">ビデオ ID。</param>
+        /// <param name="allowEdit">編集を許可するかどうか（true または false）。省略可。</param>
+        /// <param name="clientRequestId">リクエスト識別用の GUID（省略可）。</param>
+        /// <returns>アクセストークンの文字列。失敗時は null。</returns>
+        public async Task<string?> GetVideoAccessTokenAsync(string location, string accountId, string videoId, bool? allowEdit = null, string? clientRequestId = null)
+        {
+            return await _accountAccessTokensApiAccess.GetVideoAccessTokenAsync(location, accountId, videoId, allowEdit, clientRequestId);
+        }
+
     }
 }
