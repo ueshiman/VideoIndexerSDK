882c01a5194297102bea8865a5ee2a0679eb2089
tateno <masa@tateno.net>
2025-07-05 20:22:32 +0900

    ストリーミング URL マッピング機能を追加
    `VideosRepository.cs` に `IStreamingUrlMapper` を追加し、ストリーミング URL の処理を改善しました。`GetVideoStreamingUrlAsync` メソッドの戻り値の型を変更し、マッパーを使用して変換を行うようにしました。
    
    新たに `IStreamingUrlMapper` インターフェースを定義し、`StreamingUrlMapper` で実装しました。また、ストリーミング URL と JWT トークンを保持する `StreamingUrlModel` データモデルを追加しました。

---
+++ VideoIndexerAccess/Repositories/DataModelMapper/IStreamingUrlMapper.cs
@@ -0,0 +1,10 @@
+﻿using VideoIndexerAccess.Repositories.DataModel;
+using VideoIndexerAccessCore.VideoIndexerClient.ApiModel;
+
+namespace VideoIndexerAccess.Repositories.DataModelMapper;
+
+public interface IStreamingUrlMapper
+{
+    StreamingUrlModel MapFrom(ApiStreamingUrlModel model);
+    ApiStreamingUrlModel MapTo(StreamingUrlModel model);
+}
