f0d96f3fdfee6166d9b4f5c5c90aa46f38949405
tateno <masa@tateno.net>
2025-07-06 21:01:29 +0900

    ビデオプレイヤーウィジェットのレスポンス処理を追加
    `WidgetRepository` に新しいフィールドとマッピングロジックを追加し、`GetVideoPlayerWidgetAsync` メソッドの戻り値の型を変更しました。
    新しいインターフェース `IVideoPlayerWidgetResponseMapper` を定義し、マッピングを実装する `VideoPlayerWidgetResponseMapper` クラスを追加しました。
    さらに、ビデオプレイヤーウィジェットのレスポンスを表す `VideoPlayerWidgetResponseModel` クラスを新たに作成しました。

---
+++ VideoIndexerAccess/Repositories/DataModel/VideoPlayerWidgetResponseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoIndexerAccess.Repositories.DataModel
+{
+    public class VideoPlayerWidgetResponseModel
+    {
+        /// <summary>
+        /// プレイヤーウィジェットの表示URL（リダイレクト先）
+        /// </summary>
+        public string? PlayerWidgetUrl { get; set; }
+
+        /// <summary>
+        /// エラーの種類（発生した場合）
+        /// </summary>
+        public string? ErrorType { get; set; }
+
+        /// <summary>
+        /// エラーメッセージ（発生した場合）
+        /// </summary>
+        public string? Message { get; set; }
+    }
+}
---
+++ VideoIndexerAccess/Repositories/DataModelMapper/VideoPlayerWidgetResponseMapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoIndexerAccess.Repositories.DataModel;
+using VideoIndexerAccessCore.VideoIndexerClient.ApiModel;
+
+namespace VideoIndexerAccess.Repositories.DataModelMapper
+{
+    public class VideoPlayerWidgetResponseMapper : IVideoPlayerWidgetResponseMapper
+    {
+        public VideoPlayerWidgetResponseModel MapFrom(ApiVideoPlayerWidgetResponseModel model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+            return new VideoPlayerWidgetResponseModel
+            {
+                PlayerWidgetUrl = model.playerWidgetUrl,
+                ErrorType = model.errorType,
+                Message = model.message
+            };
+        }
+
+        public ApiVideoPlayerWidgetResponseModel MapToApiVideoPlayerWidgetResponseModel(VideoPlayerWidgetResponseModel model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+            return new ApiVideoPlayerWidgetResponseModel
+            {
+                playerWidgetUrl = model.PlayerWidgetUrl,
+                errorType = model.ErrorType,
+                message = model.Message
+            };
+        }
+    }
+}
