cb4c28d26fc6920619fcd7881825500296f8308a
tateno <masa@tateno.net>
2025-07-06 16:55:02 +0900

    不要なusingの削除とWidgetRepositoryの追加
    `UpdateVideoTranscriptRequestModel.cs` では、不要な `using` ステートメントが削除され、`namespace` の定義が整理されました。`UpdateVideoTranscriptRequestModel` クラスは、`VideoId` と `VttContent` のプロパティを持つシンプルなデータモデルです。
    
    `WidgetRepository.cs` では、新たに複数の `using` ステートメントが追加され、`WidgetRepository` クラスが定義されました。このクラスは、ロガーや認証トークン、アカウント情報の取得、APIリソース設定など、さまざまなインターフェースを持ち、コンストラクタでそれらを初期化します。これにより、ウィジェットに関連する機能を管理するための基盤が整えられました。

---
+++ VideoIndexerAccess/Repositories/VideoItemRepository/WidgetRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using VideoIndexerAccess.Repositories.AuthorizAccess;
+using VideoIndexerAccessCore.VideoIndexerClient.ApiAccess;
+using VideoIndexerAccessCore.VideoIndexerClient.Configuration;
+
+namespace VideoIndexerAccess.Repositories.VideoItemRepository
+{
+    public class WidgetRepository
+    {
+        // ロガーインスタンス
+        private readonly ILogger<WidgetRepository> _logger;
+
+        // アクセストークン取得用インターフェース
+        private readonly IAuthenticationTokenizer _authenticationTokenizer;
+
+        // アカウント情報取得用インターフェース
+        private readonly IAccounApitAccess _accountAccess;
+
+        // アカウント検証用リポジトリ
+        private readonly IAccountRepository _accountRepository;
+
+        // APIリソース設定
+        private readonly IApiResourceConfigurations _apiResourceConfigurations;
+
+        // API設定
+        private readonly IWidgetsApiAccess _widgetsApiAccess;
+        private const string ParamName = "widget";
+
+        // マッパーインターフェース
+
+        public WidgetRepository(ILogger<WidgetRepository> logger, IAuthenticationTokenizer authenticationTokenizer, IAccounApitAccess accountAccess, IAccountRepository accountRepository, IApiResourceConfigurations apiResourceConfigurations, IWidgetsApiAccess widgetsApiAccess)
+        {
+            _logger = logger;
+            _authenticationTokenizer = authenticationTokenizer;
+            _accountAccess = accountAccess;
+            _accountRepository = accountRepository;
+            _apiResourceConfigurations = apiResourceConfigurations;
+            _widgetsApiAccess = widgetsApiAccess;
+        }
+    }
+}
